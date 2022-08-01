94a1706e53df866edc23400cdf9db05d2de4bb36
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-08-01 15:03:45 +0000

    Add dummy test case. Rename tests to test_views. Add test files for forms and models

---
+++ todo/test_views.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+# Create your tests here.
+class TestDjango(TestCase):
+    """A class to create various test cases """
+    def test_this_thing_works(self):
+        self.assertEqual(1, 0)
