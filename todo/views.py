7493def64fd297fe2530ed9c9aa8ba82e16118b1
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-08-01 12:25:33 +0000

    Add code to create form directly from model

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 """ takes care of communication between the frontend and the backend """
 from django.shortcuts import render, redirect
 from .models import Item
+from .forms import ItemForm
 
 
 # Create your views here.
@@ -18,9 +19,15 @@ def get_todo_list(request):
 def add_item(request):
     """Provide a means for users to add items"""
     if request.method == 'POST':
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, done=done)
-
-        return redirect('get_todo_list')
-    return render(request, 'todo/add_item.html')
+        # name = request.POST.get('item_name')
+        # done = 'done' in request.POST
+        # Item.objects.create(name=name, done=done)
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
