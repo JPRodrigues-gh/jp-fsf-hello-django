61976cabc7035859dc7b377a65b3683407ef2c69
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-08-02 12:54:44 +0000

    Add models tests

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,1 +1,15 @@
+""" Views test py-file """
+
 from django.test import TestCase
+from .models import Item
+
+# Create your tests here.
+class TestModels(TestCase):
+    """  class inherits Testcase and contains all the tests on the models """
+
+    def test_done_defaults_to_false(self):
+
+        # create an item to use in this test
+        item = Item.objects.create(name='Test Todo Item')
+        # confirm that it's done status is in fact false by default
+        self.assertFalse(item.done)
