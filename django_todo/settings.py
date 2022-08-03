495c2864cf391049c40847878856b9cd2816cd85
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-08-03 11:11:42 +0000

    Remove SECRET_KEY from settings and add to GitPod and Heroku environment variables

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-%fx^72)#iupw)=nsx@i!)!7m=n&15ame^h6stk$t47)@y*ozj5')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
